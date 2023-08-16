9fc5a036b19952e913bb7730e8f18efbe7b886c5
dimavrb <dima---k@mail.ru>
2023-08-16 20:28:39 +0300

    Add HomeWork #5

---
+++ src/test/java/DragAndDropTest.java
@@ -0,0 +1,33 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class DragAndDropTest {
+
+    @BeforeAll
+    static void preconditionTests() {
+
+
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.browser = "firefox";
+        Configuration.baseUrl = "https://the-internet.herokuapp.com/";
+    }
+
+    @Test
+    void dragAndDropTest() {
+        open("drag_and_drop");
+        $("#column-a").dragAndDropTo("#column-b");
+        $(".column:first-child").shouldHave(text("B"));
+    }
+
+    @Test
+    void actionsTest() {
+        open("drag_and_drop");
+        actions().clickAndHold($("#column-a")).moveToElement($("#column-b")).release().perform();
+        sleep(5000);
+    }
+}
