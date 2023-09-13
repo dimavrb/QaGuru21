4add4802c5fa26ce54eef38d4a6c54ce31f0ab1d
dimavrb <dima---k@mail.ru>
2023-09-13 21:18:32 +0300

    Add HomeWork #12

--- src/test/java/com/demoqa/test/TestBase.java
+++ src/test/java/com/demoqa/test/TestBase.java
@@ -5,13 +5,9 @@ import com.codeborne.selenide.logevents.SelenideLogger;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import io.qameta.allure.selenide.AllureSelenide;
-import org.openqa.selenium.remote.DesiredCapabilities;
 
-import java.util.Map;
 
 import static com.codeborne.selenide.Selenide.executeJavaScript;
-import static org.junit.jupiter.params.provider.Arguments.of;
-
 public class TestBase {
     @BeforeAll
     static void beforeAll() {
@@ -19,10 +15,6 @@ public class TestBase {
         Configuration.browserSize = "1920x1080";
     //    Configuration.browser = "chrome";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("selenoid:options", Map.<~>of("enableVNC", true, "enableVideo", true));
-
     }
 
     public static void closeBanner() {
