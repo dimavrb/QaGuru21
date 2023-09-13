79baeaa53c99cec5c35aa8f5f8398481d97ae70d
dimavrb <dima---k@mail.ru>
2023-09-13 22:09:09 +0300

    Add HomeWork #12

--- src/test/java/com/demoqa/test/TestBase.java
+++ src/test/java/com/demoqa/test/TestBase.java
@@ -2,10 +2,15 @@ package com.demoqa.test;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import com.demoqa.helpers.Attach;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
+
+import java.util.Map;
 
 import static com.codeborne.selenide.Selenide.executeJavaScript;
 public class TestBase {
@@ -13,19 +18,33 @@ public class TestBase {
     static void beforeAll() {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
-    //    Configuration.browser = "chrome";
-        Configuration.pageLoadStrategy = "eager";
-    }
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
-    public static void closeBanner() {
-        executeJavaScript("$('#fixedban').remove()");
-        executeJavaScript("$('footer').remove()");
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+
+        Configuration.browserCapabilities = capabilities;
+    }
+        public static void closeBanner() {
+            executeJavaScript("$('#fixedban').remove()");
+            executeJavaScript("$('footer').remove()");
     }
 
     @BeforeEach
     void beforeEach() {
         SelenideLogger.addListener("allure", new AllureSelenide());
 
+    }
+
+    @AfterEach
+    void addAttach(){
+        Attach.screenshotAs("Attach");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
 
 }
